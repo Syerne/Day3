bc458714ffb59b6f9c903e7c2b165cf737dd93de
Syerne <shubhamyerne1994@gmail.com>
2022-06-13 18:53:02 +0530

    Added: Employee part time wage programs

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,16 +5,18 @@ public class EmpWageComputation {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
 
-        int wagePerHour = 20, fullDay = 1, empWage = 0, empHours = 0;
+        int wagePerHour = 20, fullDay = 1, empWage = 0, empHours = 0, partTime =2;
 
 
-        int randomNum = (int) (Math.random() * 10 % 2);
+        int randomNum = (int) (Math.random() * 10 % 3);
         System.out.println("RandomNumGenerate: " + randomNum);
 
         if (randomNum == fullDay) {
             empHours = 8;
-        } else {
-            empHours = 0;
+        } else if(randomNum == partTime){
+            empHours = 4;
+        }else {
+            empHours=0;
         }
         empWage = (wagePerHour * empHours);
         System.out.println("Employee Wages is: " + empWage);
