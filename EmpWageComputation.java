cc6a4d0bd8c64511321bff4f8b042b6a8d9a47af
Syerne <shubhamyerne1994@gmail.com>
2022-06-13 16:55:56 +0530

    Added: welcome message

---
+++ EmpWageComputation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day3;
+
+public class EmpWageComputation {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee wage computation program");
+    }
+}
