494a0318a485239fa5cf64ed64cc12f115125a35
Syerne <shubhamyerne1994@gmail.com>
2022-06-13 18:44:22 +0530

    Added: Employee daily wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,16 +1,22 @@
 package com.bridgelabz.day3;
 
 public class EmpWageComputation {
+
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
 
-        int randomNum = (int) ( Math.random()*10 % 2);
+        int wagePerHour = 20, fullDay = 1, empWage = 0, empHours = 0;
+
+
+        int randomNum = (int) (Math.random() * 10 % 2);
         System.out.println("RandomNumGenerate: " + randomNum);
 
-        if(randomNum == 1){
-            System.out.println("Employee is present");
+        if (randomNum == fullDay) {
+            empHours = 8;
         } else {
-            System.out.println("Employee is absent");
+            empHours = 0;
         }
+        empWage = (wagePerHour * empHours);
+        System.out.println("Employee Wages is: " + empWage);
     }
 }
