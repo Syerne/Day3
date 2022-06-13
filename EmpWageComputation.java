b8669781dd3bc429e0d39cdce2bf2bd92b63d75a
Syerne <shubhamyerne1994@gmail.com>
2022-06-13 19:09:00 +0530

    Added: Employee wage calculate using Switch case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,20 +5,29 @@ public class EmpWageComputation {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
 
-        int wagePerHour = 20, fullDay = 1, empWage = 0, empHours = 0, partTime =2;
+        int wagePerHour = 20, fullDay = 1, empWage = 0, empWorkingHours = 0, partTime =2;
 
 
         int randomNum = (int) (Math.random() * 10 % 3);
         System.out.println("RandomNumGenerate: " + randomNum);
 
-        if (randomNum == fullDay) {
-            empHours = 8;
-        } else if(randomNum == partTime){
-            empHours = 4;
-        }else {
-            empHours=0;
+        switch (randomNum){
+            case 1:
+                System.out.println("Employee is full time");
+                empWorkingHours=8;
+                break;
+            case 2:
+                System.out.println("Employee is part time");
+                empWorkingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+                empWorkingHours=0;
+                break;
         }
-        empWage = (wagePerHour * empHours);
+
+
+        empWage = (wagePerHour * empWorkingHours);
         System.out.println("Employee Wages is: " + empWage);
     }
 }
