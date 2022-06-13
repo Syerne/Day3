ae70a2090aa85f4609982fb1f8431f5d07966c33
Syerne <shubhamyerne1994@gmail.com>
2022-06-13 18:18:13 +0530

    Added: Employee attendence check program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,5 +3,14 @@ package com.bridgelabz.day3;
 public class EmpWageComputation {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
+
+        int randomNum = (int) ( Math.random()*10 % 2);
+        System.out.println("RandomNumGenerate: " + randomNum);
+
+        if(randomNum == 1){
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
